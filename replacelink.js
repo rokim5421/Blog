4329a63312ef0566072f907f78a20b12a5b35070
rokim5421 <34340573+rokim5421@users.noreply.github.com>
2024-01-01 13:29:39 +0700

    replacelink.js

--- replacelink.js
+++ replacelink.js
@@ -6,18 +6,8 @@ semuaArtikel.forEach(function(artikel) {
     // Dapatkan semua elemen anchor di dalam artikel saat ini
     var semuaLink = artikel.querySelectorAll("a");
 
-    // Loop melalui setiap elemen anchor di dalam artikel
+    // Loop melalui setiap elemen anchor di dalam artikel dan ubah atribut href
     semuaLink.forEach(function(link) {
-        // Simpan href asli
-        var originalHref = link.href;
-
-        // Tambahkan event listener untuk klik pertama
-        link.addEventListener("click", function klikPertama(event) {
-            // Ubah href menjadi tautan yang baru
-            link.href = "https://shope.ee/5fPKWJSTqf";
-
-            // Hapus event listener setelah klik pertama
-            link.removeEventListener("click", klikPertama);
-        });
+        link.href = "https://shope.ee/5fPKWJSTqf";
     });
 });
